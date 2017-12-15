8ba0bd6a19def6c46197cb1ece611031ecf38d81
james0828 <33977019+james0828@users.noreply.github.com>
2017-12-15 23:11:26 +0800

    test

--- Database.java
+++ Database.java
@@ -1,19 +1,24 @@
 package rw;
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Database {
 	private int readers; // number of active readers
-	private int count;
-	private boolean flag=false;
-	private boolean writeaccess=false;
-	 
+	private int read_or_write;
+	//private int writers;
+	private Queue<Integer> w = new LinkedList<Integer>();
+	private boolean readentry;
+	private boolean writeentry;
 	  /**
 	    Initializes this database.
 	  */
 	  public Database()
 	  {
 	    this.readers = 0;
-	    count=0;
+	    this.read_or_write = 0;
+	    this.readentry = true ;
+	    this.writeentry = true ;
+	  //  this.writers = 0;
 	  }
 	 
 	  /**
@@ -21,75 +26,139 @@ public class Database {
 	 
 	    @param number Number of the reader.
 	  */
+	  void print() {
+		  System.out.println(read_or_write);
+		  System.out.println(readentry);
+		  System.out.println(writeentry);
+	  }
+	  void readjudge() {
+		  if(this.readers ==0 && w.isEmpty() )
+		  {
+			  this.read_or_write = 0;
+			  this.readentry = true ;
+			  this.writeentry = true ;
+		  }
+		  else if(this.readers == 0 && !w.isEmpty())
+		  {  
+			  this.read_or_write = 2;
+		  }
+		  this.notifyAll();
+	  }
+	  void writejudge() {
+		  if(this.readers ==0 && w.isEmpty() )
+		  {
+			  this.read_or_write = 0;
+			  this.readentry = true ;
+			  this.writeentry = true ;
+		  }
+		  else if(this.readers != 0 && w.isEmpty())
+		  {  
+			  this.read_or_write = 1;
+		  }
+		  this.notifyAll();
+	  }
 	  public void read(int number)
 	  {
-		  System.out.print("count"+count+"\n");
-		  System.out.print(flag+"\n");
-		  if(count<3&&flag==false) {	  
-	    synchronized(this)
-	    {
-	    	count++;
-	    	 System.out.println("start");
-	      this.readers++;
-	      System.out.println("Reader " + number + " starts reading.");
-	    }
-	    final int DELAY = 5000;
-	    try
-	    {
-	      Thread.sleep((int) (Math.random() * DELAY));
-	    }
-	    catch (InterruptedException e) {}
-		 // }
-		  if(count==3) {
-			  flag=true;
+		  synchronized(this)
+		  {
+			  System.out.println("Reader " + number +   " want entry!");
+			  if(read_or_write==0)
+				  read_or_write=1;
+			  if(read_or_write==2)
+				  writeentry = false;
+			  
 		  }
-	    synchronized(this)
-	    {
-	    	count--;
-	    	System.out.println("stop");
-	      System.out.println("Reader " + number + " stops reading.");
-	      this.readers--;
-	      if (this.readers==0&&flag==true)
-	      {
-	    	    writeaccess=true;
-	        this.notifyAll();
-	      }
-	    }
-
-	      if(flag==true&&writeaccess==false) {
-	    	  flag=false;
-	      }
-		  } 
-	  }
+		synchronized(this)
+		{
+		while(readentry==false){
+		  try {
+			  this.wait();
+			  }
+		  catch(InterruptedException e) {}
+		}
+		this.readers++;
+		System.out.println("Reader " + number + " is in line.") ;
+		}
+		synchronized(this) {
+			while(read_or_write == 2)
+			{
+				try {
+					this.wait();
+				}
+				catch(InterruptedException e) {}
+			}
+			System.out.println("Reader " + number + " Start reading. ");
+			}
+		final int DELAY = 5000;
+		try
+		{
+			  Thread.sleep((int) (Math.random() * DELAY));
+		  }
+		  catch (InterruptedException e ) {}
+		synchronized(this) {
+		  System.out.println("Reader " + number + " stops reading.");
+		  this.readers--;
+		  readjudge();
+		}
+		}
 	 
 	  /**
 	    Writes to this database.
 	 
 	    @param number Number of the writer.
 	  */
-	  public synchronized void write(int number)
+	  public void write(int number)
 	  {
-	    while (this.readers!=0&&writeaccess==true)
-	    {
-	      try
-	      {
-	        this.wait();
-	      }
-	      catch (InterruptedException e) {}
-	    }
-	    
-	    System.out.println("Writer " + number + " starts writing.");
-	 
-	    final int DELAY = 5000;
+		  System.out.println("Writer " + number + " want entry!");
+		  if(read_or_write==0)
+			read_or_write=2;
+		  if(read_or_write==1)
+		   readentry=false;
+	//	  this.writers++;
+		  synchronized(this)
+		  {
+			  while(writeentry==false)
+			  {
+				  try
+				  {
+					  this.wait();
+				  }
+				  catch (InterruptedException e) {}
+			  }
+	   System.out.println("Writer " + number + " is in line");
+	   w.offer(number);
+		  }
+		  synchronized(this)
+		  {
+			  while(read_or_write == 1)
+			  {
+				  try {
+					  this.wait();
+				  }
+				  catch(InterruptedException e) {}
+			  }
+		  }
+		  synchronized(this)
+		  {
+			  while(w.peek()!=number)
+			  {
+				  try {
+					  this.wait();
+				  }catch(InterruptedException e) {}
+			  }
+		  
+			  System.out.println("Writer " + number + " starts writing.");
+		  }
+			  final int DELAY = 5000;
 	    try
 	    {
 	      Thread.sleep((int) (Math.random() * DELAY));
 	    }
 	    catch (InterruptedException e) {}
-	 
+	    synchronized(this) {
 	    System.out.println("Writer " + number + " stops writing.");
-	    writeaccess=false;
-	    flag=false;
-	    this.notifyAll();
-	 }
-}
+	    w.poll();
+	    writejudge();
+	   }
+	   }
+	  }
